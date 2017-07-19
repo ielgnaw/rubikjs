238dc3f29367f2400b13e346691cc5c029db0164
ielgnaw <wuji0223@gmail.com>
2017-07-19 23:14:35 +0800

    backup

--- example/index.js
+++ example/index.js
@@ -1,4 +1,111 @@
 
+import {join} from 'path';
 import Rubik from '../src';
-const rubik = new Rubik();
-rubik.start('https://www.baidu.com');
+const rubik = new Rubik({
+    headless: false
+});
+
+// rubik.start('file://' + join(__dirname, '/page/index.html'))
+//     .getTitle()
+//     .result(function () {
+//         console.log(arguments);
+//     })
+
+// 回调函数形式
+// rubik.start('file://' + join(__dirname, '/page/index.html'), () => {
+    // 回调函数形式
+    // rubik.getTitle(ret => {
+    //     console.log('example', ret);
+    // });
+
+    // promise then 形式
+    // rubik.getTitle().then(ret => {
+    //     console.log('111', ret);
+    // });
+// });
+
+// promise then 形式
+// rubik.start('file://' + join(__dirname, '/page/index.html')).then(() => {
+    // 回调函数形式
+    // rubik.getTitle(ret => {
+    //     console.log('example', ret);
+    // });
+
+    // promise then 形式
+    // rubik.getTitle().then(ret => {
+    //     console.log('111', ret);
+    // });
+// });
+
+// async await 形式
+// async function main() {
+//     await rubik.start('file://' + join(__dirname, '/page/index.html'));
+//     // async await 形式
+//     const ret = await rubik.getTitle();
+//     console.log(ret);
+
+//     // promise then 形式
+//     rubik.getTitle().then(ret => {
+//         console.log('111', ret);
+//     });
+
+//     // 回调函数形式
+//     rubik.getTitle(ret => {
+//         console.log('example', ret);
+//     });
+// }
+// main();
+
+
+// console.log(rubik);
+// console.log(Rubik.addCustomDevice);
+// async function main() {
+    // rubik.start('file://' + join(__dirname, '/page/index.html'));
+    // const ret = rubik.getTitle().then(function () {console.log(arguments);});
+    // console.log(ret);
+// }
+// main();
+
+// const result = await chromy.evaluate(() => {
+//         return document.querySelectorAll('*').length
+//     });
+
+
+// import fs from 'fs';
+// import Chromy from 'chromy';
+
+// async function main() {
+//     // let chromy = new Chromy({
+//     //     // visible: true
+//     // });
+//     // await chromy.goto('http://ielgnaw.com/')
+//     // const result = await chromy.evaluate(() => {
+//     //     return document.querySelectorAll('*').length
+//     // });
+//     // console.log(result)
+//     // await chromy.close();
+
+//     let chromy = new Chromy()
+//     chromy.chain()
+//       .goto('https://github.com')
+//       .screenshot()
+//       .result((png) => {
+//         fs.writeFileSync('out.png', png)
+//       })
+//       .screenshotDocument() // take screenshot of whole document
+//       .result((png) => {
+//         fs.writeFileSync('out_doc.png', png)
+//       })
+//       .pdf()
+//       .result((pdf) => {
+//         fs.writeFileSync('out.pdf', pdf)
+//       })
+//       .end()
+//       .then(_ => chromy.close())
+//       .catch(e => {
+//         console.log(e)
+//         chromy.close()
+//       })
+// }
+
+// main()
--- src/index.js
+++ src/index.js
@@ -5,89 +5,238 @@
 
 import 'babel-polyfill';
 import Debug from 'debug';
-import CDP from 'chrome-remote-interface';
+import Chromy from 'chromy';
+// import chainProxy from 'async-chain-proxy';
 
 import {mergeDeep} from './util';
 
 const debug = Debug('rubik');
 
 /**
- * default target
- * from chrome-remote-interface/lib/chrome.js
+ * 获取 type 为 page 的 target，可理解为获取所访问页面的那个 tab，为了之后和这个 tab 通过 ws 通信
  *
- * @param {Array} targets targets
+ * @param {Array} targets all targets
  *
- * @return {Object} target
+ * @return {Object} target info
  */
-const defaultTarget = targets => {
-    let backup;
-    let target = targets.find(target => {
-        if (target.webSocketDebuggerUrl) {
-            backup = backup || target;
-            // http://localhost:9222/json 获取到访问的页面，t.type === 'page'
-            // type 可选值为 background_page，service_worker
-            return target.type === 'page';
-        }
-        return false;
-    });
-
-    target = target || backup;
-
-    if (target) {
-        return target;
-    }
-
-    throw new Error('No inspectable targets');
-};
-
-const instances = [];
-let instanceId = 0;
+function defaultTargetFunction(targets) {
+    return targets.filter(t => t.type === 'page').shift();
+}
 
 const defaultOpts = {
-    // all flags: http://peter.sh/experiments/chromium-command-line-switches/
-    chromeFlags: ['--disable-gpu'],
-    chromePath: null,
-    userDataDir: null,
-    startUrl: null,
+    host: 'localhost',
+    port: 9222,
     launchBrowser: true,
-    // verbose, info, error, silent
-    logLevel: 'error',
-    cdpOpts: {
-        port: 9222,
-        host: 'localhost',
-        target: defaultTarget,
-        onError: function () {}
-    }
+    chromeFlags: [],
+    chromePath: null,
+    activateOnStartUp: true,
+    waitTimeout: 30000,
+    gotoTimeout: 30000,
+    loadTimeout: 30000,
+    evaluateTimeout: 30000,
+    waitFunctionPollingInterval: 100,
+    typeInterval: 20,
+    target: defaultTargetFunction,
+    // 是否使用 headless 模式
+    headless: false
 };
+
+async function sleep(fn, timestamp, ...args) {
+    await new Promise(resolve => setTimeout(resolve, timestamp));
+    return fn(...args);
+    // return await new Promise(resolve => setTimeout(resolve, timestamp));
+}
+
+// sleep(function () {
+//     console.log(arguments, 'done');
+// }, 100, {a: 1})
+
+class Test {
+    constructor(promise) {
+        this.$promise = promise || Promise.resolve();
+        this._queue = [];
+    }
+
+    async then(fn) {
+        // const s = await this._queue[0]();
+        // console.log(s);
+
+        // return Promise.all([this._queue[0](), this._queue[1](), this._queue[2]()]).then(function (resolve, reject) {
+        //     console.log(22, arguments);
+        // });
+
+        const steps = this._queue.concat();
+        const me = this;
+
+        async function next(ret = {}) {
+            const item = steps.shift();
+            if (!item) {
+                return await done(ret);
+            }
+            const s = await item();
+            return await after(s);
+        }
+
+        async function after(ret) {
+            return await next(ret);
+        }
+
+        async function done(ret) {
+            return fn.apply(me, [ret]);
+        }
+
+        next();
+    }
+
+    catch(reject) {
+        return this.then(reject);
+    }
+
+    thing1(fn) {
+        const startTime = +new Date;
+        this._queue.push(() => sleep(function () {
+            const endTime = +new Date;
+            console.log(arguments, 'thing1', endTime - startTime);
+            return endTime - startTime;
+        }, 5000, {a: 1}));
+        return this;
+        // return this.then(() => sleep(function () {
+        //     const endTime = +new Date;
+        //     console.log(arguments, 'thing1', endTime - startTime);
+        // }, 5000, {a: 1}));
+    }
+
+    thing2(fn) {
+        const startTime = +new Date;
+        this._queue.push(() => sleep(function () {
+            const endTime = +new Date;
+            console.log(arguments, 'thing2', endTime - startTime);
+            return endTime - startTime;
+        }, 2000, {a: 2}));
+        return this;
+        // return this.then(() => sleep(function () {
+        //     const endTime = +new Date;
+        //     console.log(arguments, 'thing2', endTime - startTime);
+        // }, 2000, {a: 2}));
+    }
+
+    thing3(fn) {
+        const startTime = +new Date;
+        this._queue.push(() => sleep(function () {
+            const endTime = +new Date;
+            console.log(arguments, 'thing3', endTime - startTime);
+            return endTime - startTime;
+        }, 3000, {a: 3}));
+        return this;
+        // return this.then(() => sleep(function () {
+        //     const endTime = +new Date;
+        //     console.log(arguments, 'thing3', endTime - startTime);
+        // }, 3000, {a: 3}));
+    }
+}
+
+new Test(Promise.resolve())
+    .thing1(function (_id) {
+        console.log(11111, arguments);
+    })
+    .thing2(function (_id) {
+        console.log(22222, arguments);
+    })
+    .thing3(function (_id) {
+        console.log(33333, arguments);
+    })
+    // .then(function () {
+    //     console.log(111, arguments);
+    // })
+    .catch(error => console.error(error));
+
 
 export default class Rubik {
     constructor(opts = {}) {
         // debug('send "%s" %j', 'path', {a: 1});
         this.opts = mergeDeep({}, defaultOpts, opts);
+        this.opts.visible = !this.opts.headless;
 
-        this.instanceId = instanceId++;
+        this.client = new Chromy(this.opts);
+
+        this._queue = [];
     }
 
-    async start(url = 'about:blank') {
-        const cdpOpts = this.opts.cdpOpts;
-
-        // CDP 是需要 launch browser 的，只是在这里可以在别处启动这个 browser 而不一定非要在程序里启动
-        // 例如可以在另一个命令行运行 chrome --remote-debugging-port=9222 然后再运行 rubikjs
-        if (this.opts.launchBrowser) {
-        }
-
-        await new Promise((resolve, reject) => {
-            CDP(cdpOpts, async protocol => {
-                console.log(protocol);
-                resolve(protocol);
-            }).on('error', err => {
-                console.log(123);
-                cdpOpts.onError(err);
-                reject(err);
-            });
-        }).catch(e => {
-            console.log(44);
-            throw e;
-        });
+    /**
+     * 添加自定义的设备
+     * {name, width, height, deviceScaleFactor, pageScaleFactor, mobile, userAgent}
+     *
+     * @param {Array|Object} cusDevices 设备信息
+     */
+    static addCustomDevice(cusDevices = []) {
     }
+
+    addQueue(fn) {
+        this._queue.push(fn);
+    }
+
+    result(fulfill, reject) {
+        return new Promise((success, failure) => {
+            this.run((err, result) => {
+                if (err) {
+                    failure(err);
+                }
+                else {
+                    success(result);
+                }
+            })
+        }).then(fulfill, reject);
+    }
+
+    run(fn) {
+        debug('running');
+    }
+
+    // start(startingUrl, headers) {
+    //     debug(`queueing action 'start' for ${startingUrl}` );
+    //     headers = headers || {};
+    //     for (var key in this._headers) {
+    //         headers[key] = headers[key] || this._headers[key];
+    //     }
+
+    //     this.addQueue(() => {
+    //         this.client.start(startingUrl);
+    //         // self.child.call('goto', url, headers, this.options.gotoTimeout, fn);
+    //     });
+
+    //     return this;
+    // }
+
+    // getTitle() {
+    //     this.addQueue(async () => {
+    //         console.log(123123);
+    //         await this.client.evaluate(_ => {
+    //             console.log(9999);
+    //             return document.title;
+    //         })
+    //     });
+    //     return this;
+    // }
+
+    // async start(startingUrl = 'about:blank', callback = null) {
+    //     const ret = await this.client.start(startingUrl);
+    //     if (callback && typeof callback === 'function') {
+    //         (async () => {
+    //             callback.call(this, ret);
+    //         })();
+    //     }
+    //     return this;
+    // }
+
+    // async getTitle(callback = null) {
+    //     const ret = await this.client.evaluate(_ => {
+    //         return document.title;
+    //     });
+    //     if (callback && typeof callback === 'function') {
+    //         (async () => {
+    //             callback.call(this, ret);
+    //         })();
+    //     }
+    //     return ret;
+    // }
 }
